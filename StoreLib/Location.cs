f42749852c90155e3839bbb4a2ff760d202b2086
lw3b3r <daevahush@gmail.com>
2020-10-25 11:20:30 -0400

    test

--- StoreLib/Location.cs
+++ StoreLib/Location.cs
@@ -2,6 +2,11 @@ namespace StoreLib
 {
     public class Location
     {
-        
+        int id { get; set; }
+        string street { get; set; }
+        string city { get; set; }
+        string state { get; set;}
+        string postalCode { get; set; }
+
     }
 }
