6cea3c41066f1340e73675742f5446a0d0e6e8dd
lw3b3r <daevahush@gmail.com>
2020-10-25 17:14:11 -0400

    feat: Corrected Order model
    Added field for customer Id to Order model

--- StoreLib/Models/Order.cs
+++ StoreLib/Models/Order.cs
@@ -3,12 +3,14 @@ namespace StoreLib.Models
     public class Order
     {
         int id { get; set; }
+        int userId { get; set; }
         string[] lineItem { get; set; }
 
         Order() {}
 
-        Order(int id, string[] lineItem) {
+        Order(int id, int userId, string[] lineItem) {
             this.id = id;
+            this.userId = userId;
             this.lineItem = lineItem;
         }
     }
