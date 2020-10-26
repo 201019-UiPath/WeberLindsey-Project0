963e392154757463091e0c3782d33b2744ca64c5
lw3b3r <daevahush@gmail.com>
2020-10-26 13:58:30 -0400

    Created Welcome menu

--- StoreUI/Menus/WelcomeMenu.cs
+++ StoreUI/Menus/WelcomeMenu.cs
@@ -10,8 +10,7 @@ namespace StoreUI.Menus
         public void Start() {
             string userInput;
             do{
-                System.Console.WriteLine("Welcome to CF Books!");
-                System.Console.WriteLine("Please select an option: ");
+                System.Console.WriteLine("Welcome to CF Books! Please select an option: ");
 
                 //Menu Options
                 System.Console.WriteLine("[0] Sign In \n[1] Sign Up \n[3] Exit");
