b960f2e8d9963e454ca24cbc3f2f75d99f7ddf77
lw3b3r <daevahush@gmail.com>
2020-10-26 18:42:07 -0400

    test

--- StoreLib/Models/User.cs
+++ StoreLib/Models/User.cs
@@ -1,7 +1,10 @@
+using System;
+
 namespace StoreLib
 {
     public class User
     {
+        //TODO include defaultLocation property? or have customers select it upon login?
         private int id { get; set; }
         private string name { get; set; }
         private string email { get; set; }
@@ -21,6 +24,24 @@ namespace StoreLib
         }
 
         User() {}
+
+        User(string name, string email, string username, string password, userType type) {
+            this.name = name;
+            this.email = email;
+            this.username = username;
+            this.password = password;
+            this.type = type;
+        }
+
+        User(string name, string email, string username, string password, string[] orders, string[] cart, userType type) {
+            this.name = name;
+            this.email = email;
+            this.username = username;
+            this.password = password;
+            this.orders = orders;
+            this.cart = cart;
+            this.type = type;
+        }
 
         User(int id, string name, string email, string username, string password, userType type) {
             this.id = id;
@@ -42,8 +63,49 @@ namespace StoreLib
             this.type = type;
         }
 
-        public abstract void SignIn() {}
-        public abstract void SignUp() {}
+        public void SignIn(string username, string password) {
+            System.Console.WriteLine("Enter username: ");
+            username = Console.ReadLine();
+
+            System.Console.WriteLine("Enter password: ");
+            password = Console.ReadLine();
+
+            //TODO Create login validation
+            //If validation successful, 
+             
+            //Direct to customer or manager menu depending on userType TODO relocate this to menu and instead have this method return a boolean
+            switch(userType) {
+                case Manager: 
+                    IMenu startMenu = new ManagerMenu();
+                    startMenu.Start();
+
+                case Customer:
+                    IMenu startMenu = new CustomerMenu();
+                    startMenu.Start();
+             }}
+             
+        public void SignUp(string name, string email, string username, string password, Type userType) {
+            System.Console.WriteLine("Please provide your first and last name: ");
+            name = Console.ReadLine();
+
+            System.Console.WriteLine("Please provide your email address: ");
+            email = Console.ReadLine();
+
+            System.Console.WriteLine("Choose a username: ");
+            username = Console.ReadLine();
+
+            System.Console.WriteLine("Create a password: ");
+            password = Console.ReadLine();
+
+            //TODO create validation for username and email addresses
+            //If validation passes,
+            //Edit this to include the userType
+            User newUser = new User(name, email, username, password, User.userType.Customer);
+            System.Console.WriteLine("User account created successfully!\n");
+        }
+
+        public void CreateManager() {}
+        public void DeleteManager() {}
 
     }
 }
